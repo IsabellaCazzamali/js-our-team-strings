c87e056a5f3188c7edbbaa86efba71bf9d68af82
IsabellaCazzamali <isa.cazzamali@gmail.com>
2023-12-16 12:04:02 +0100

    Initial commit
    Created team members array
    
    Milestone 0

---
+++ script.js
@@ -0,0 +1,26 @@
+/* 
+Utilizzando i dati forniti, creare un array di stringhe con i nomi dei membri del team.
+MILESTONE 0:
+Creare l’array di stringhe contenente i nomi dei membri del team.
+MILESTONE 1:
+Stampare le informazioni su DOM come card.
+MILESTONE 3:
+Aggiungere un evento click sulla card che aggiunge/rimuove una classe per evidenziare un componente del team.
+BONUS 1:
+In generale curare la parte di UI e organizzare i singoli membri in card/schede.
+BONUS 2:
+Inserire un form per l’aggiunta di un elemento alla lista.
+Consigli del giorno: Ragioniamo come sempre a step. Prima la logica in italiano e poi traduciamo in codice.
+E ricordiamoci che console.log() è nostro amico!
+*/
+
+// Array dei membri del gruppo
+const members = [
+    'Tony Stark',
+    'Bruce Banner',
+    'Thor',
+    'Steve Rogers',
+    'Natasha Romanoff',
+    'Clint Barton'
+];
+
